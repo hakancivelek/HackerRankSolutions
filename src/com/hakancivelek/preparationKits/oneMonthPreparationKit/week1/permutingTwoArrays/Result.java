15e1db0d052e37ce53dd5411fc374625108514a5
Hakan CİVELEK <hakancivelek5461@gmail.com>
2025-02-05 11:29:03 +0300

    week1 Permuting Two Arrays solved!

---
+++ src/com/hakancivelek/preparationKits/oneMonthPreparationKit/week1/permutingTwoArrays/Result.java
@@ -0,0 +1,36 @@
+package com.hakancivelek.preparationKits.oneMonthPreparationKit.week1.permutingTwoArrays;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Determines whether two arrays can be permuted such that for every index i, A[i] + B'[i] >= k.
+ *
+ * @param minRequiredSum The required minimum sum for each index.
+ * @param list1 The first list of integers.
+ * @param list2 The second list of integers.
+ * @return "YES" if such a permutation exists, otherwise "NO".
+ */
+class Result {
+
+    public static String twoArrays(int minRequiredSum, List<Integer> list1, List<Integer> list2) {
+        Collections.sort(list1);
+        Collections.sort(list2);
+
+        int size = list2.size();
+        List<Integer> reverseB = new ArrayList<>(size);
+        for (int i = size - 1; i >= 0; i--) {
+            reverseB.add(list2.get(i));
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (list1.get(i) + reverseB.get(i) < minRequiredSum) {
+                return "NO";
+            }
+        }
+
+        return "YES";
+    }
+}
---
+++ src/com/hakancivelek/preparationKits/oneMonthPreparationKit/week1/permutingTwoArrays/Solution.java
@@ -0,0 +1,32 @@
+package com.hakancivelek.preparationKits.oneMonthPreparationKit.week1.permutingTwoArrays;
+
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int q = scanner.nextInt();
+
+        while (q-- > 0) {
+            int n = scanner.nextInt();
+            int k = scanner.nextInt();
+
+            List<Integer> A = new ArrayList<>();
+            List<Integer> B = new ArrayList<>();
+
+            for (int i = 0; i < n; i++) {
+                A.add(scanner.nextInt());
+            }
+
+            for (int i = 0; i < n; i++) {
+                B.add(scanner.nextInt());
+            }
+
+            String result = Result.twoArrays(k, A, B);
+            System.out.println(result);
+        }
+
+        scanner.close();
+    }
+}
